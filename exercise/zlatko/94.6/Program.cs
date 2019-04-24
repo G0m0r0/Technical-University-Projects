6848ea7fc900a7c92c35a4efd78cb6814bc62d8d
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2019-04-24 14:45:19 +0300

    not finished 94.6

--- exercise/zlatko/94.6/Program.cs
+++ exercise/zlatko/94.6/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 
+//page 94 exercise 6
+//2D array with apples
+//not finished
 namespace _94._6
 {
-    //page 94 exercise 
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter M: ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] apples = new int[n, m];
 
+            InitializeCountOfApples(apples);
+
+            PrintApples(apples);
+
+
+        }
+
+        private static void PrintApples(int[,] apples)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InitializeCountOfApples(int[,] apples)
+        {
+            throw new NotImplementedException();
         }
     }
 }
