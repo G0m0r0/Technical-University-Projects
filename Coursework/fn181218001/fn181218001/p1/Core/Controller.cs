34ffbf0d2e99e057e50a911a9b77d7cf472207e0
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-04-17 19:41:27 +0300

    refactor coursework added some unit tests

---
+++ Coursework/fn181218001/fn181218001/p1/Core/Controller.cs
@@ -0,0 +1,103 @@
+﻿namespace p1.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class Controller
+    {
+        private List<int> sequence;
+
+        private const int lengthOfSequence = 10;
+        private const int start = 0;
+        private const int end = 100;
+        public Controller()
+        {
+            sequence = new List<int>();
+        }
+        public string Gen()
+        {
+            Random rnd = new Random();
+            sequence.Clear();
+
+            for (int i = 0; i < lengthOfSequence; i++)
+            {
+                sequence.Add(rnd.Next(start, end));
+            }
+
+            return parseSequenceToString(sequence);
+        }
+
+        public string Save(string fileName)
+        {
+            validateTxtFile(fileName);
+
+            using (StreamWriter sw = new StreamWriter($@"D:\Programming\University\Coursework\fn181218001\{fileName}"))
+            {
+                sw.WriteLine(parseSequenceToString(sequence));
+            }
+
+            return "Successfully saved!";
+        }
+
+        public string Load(string fileName)
+        {
+            validateTxtFile(fileName);
+
+            var input = File.ReadAllLines(@$"D:\Programming\University\Coursework\fn181218001\{fileName}");
+            sequence.Clear();
+
+            //lines of .txt file
+            for (int i = 0; i < input.Length; i++)
+            {
+                var nums = input[0].Split(", ").Select(int.Parse).ToArray();
+                sequence.AddRange(nums);
+            }
+
+            return parseSequenceToString(sequence);
+        }
+
+        public string View => parseSequenceToString(sequence);
+
+        public string Sort()
+        {
+            sequence.Sort();
+
+            return parseSequenceToString(sequence);
+        }
+
+        public string SortAndSaveToAnotherFile(string fileToRead, string fileToWrite)
+        {
+            Load(fileToRead);
+
+            sequence.Sort();
+
+            Save(fileToWrite);
+
+            return "New sorted file was created!";
+        }
+
+        private void validateTxtFile(string fileName)
+        {
+            if (!(fileName.EndsWith(".txt") && fileName.Length > 4&&!String.IsNullOrWhiteSpace(fileName)))
+            {
+                throw new Exception("Invalid file name");
+            }
+        }
+
+        private string parseSequenceToString(List<int> sequence) => string.Join(", ", sequence);
+
+        public string HelpCommand()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"To generate random sequence of {lengthOfSequence} numbers (-g <file name>)");
+            sb.AppendLine("To view values from file (-v <file name>)");
+            sb.AppendLine("To read numbers from one file and save in another sorted (-s <file to read> <file to sort>)");
+            sb.AppendLine("To exit (-e )");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Coursework/fn181218001/fn181218001/p1/Core/Engin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace p1.Core
+{
+    public class Engin
+    {
+        private Controller controller;
+
+        public Engin()
+        {
+            this.controller = new Controller();
+        }
+
+        public void Run(string[] args)
+        {
+            Console.WriteLine("Type -h for all commands:");
+            try
+            {
+                while (true)
+                {
+                    string result = string.Empty;
+
+                    if (args.Length == 0)
+                    {
+                        args = Console.ReadLine().Split(" ");
+                    }
+
+                    switch (args[0])
+                    {
+                        case "-h":
+                            result=this.controller.HelpCommand();
+                            break;
+                        case "-g":
+                            result=this.controller.Gen()+"\n";
+                            result+=this.controller.Save(args[1]);
+                            break;
+                        case "-v":
+                            result=this.controller.Load(args[1]);
+                            break;
+                        case "-s":
+                            result=this.controller.SortAndSaveToAnotherFile(args[1], args[2]);
+                            break;
+                        case "-e":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            result="Invalid command!";
+                            break;
+                    }
+
+                    Console.WriteLine(result);
+                    args = Console.ReadLine().Split(" ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
